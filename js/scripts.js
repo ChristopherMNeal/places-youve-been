2817342c27da3858cbe9284a4bfa28427592a378
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-01 13:51:16 -0700

    Add UI logic
    Co-authored-by: Aaron Minnick <abminnick@gmail.com>
    Co-Authored-by: Skylar Brockbank <skylar.brockbank@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+//Utility functions
+function toList(array) {
+  let output = array.map(function(element) {
+    return "<li>" + element + "</li>";
+  });
+  return output.join();
+}
+
+// object functions
 function Place({name = "", location = "", landmarks = [], timeOfYear = "", notes = []}) {
   this.name = name;
   this.location = location;
@@ -20,14 +29,48 @@ Place.prototype.addNote = function(note) {
   this.notes.push(note);
 };
 
+Place.prototype.generateCard = function() {
+  return "<div class='place-card'><h3 class='card-title'>" + this.name + "</h3><div class='hidden'><h5 class= 'location'>" + this.location + "</h5><p class='time-of-year'>" + this.timeOfYear + "</p><ul class= 'landmarks'>" + toList(this.landmarks) + "</ul><ul class='notes'>" + toList(this.notes) + "</ul></div></div>";
+}
+
+function TomeOfPlaces() {
+  this.contents = [];
+}
+
+TomeOfPlaces.prototype.addPlace = function(place){
+  this.contents.push(place);
+};
 
 
-portland = new Place({name : 'Portland', location : 'OR', timeOfYear : "August"});
+//misc for now
 
-portland.addNote("Winter is rainy");
-portland.notes[0];
-portland.addNote(true);
+let portland = new Place({name : 'Portland', location : 'OR', timeOfYear : "August", notes:["Winter is rainy"]});
 
-$(".place-card").click(function() {
-  this.firstChild.slideToggle();
+let portland2AttackOfTheClones = new Place({name : 'Portland2', location : 'OR', timeOfYear : "August", notes:["Winter is rainy(I hate sand...)"]});
+
+let portland3TokyoDrift = new Place({name : 'Portland3', location : 'OR', timeOfYear : "August", notes:["Winter is rainy (dorrriiiffto)"]});
+
+let theBook = new TomeOfPlaces();
+
+theBook.addPlace(portland);
+theBook.addPlace(portland2AttackOfTheClones);
+theBook.addPlace(portland3TokyoDrift);
+
+
+
+
+
+
+
+// UI Logic
+$(document).ready(function() {
+  for (let i of theBook.contents) {
+    $(".box").append(i.generateCard());
+  }
+  
+
+  $(".card-title").click(function() {
+    $(this.nextElementSibling).slideToggle();
+  });
 });
+
